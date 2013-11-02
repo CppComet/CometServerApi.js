f5e8fbf3151234c84418b062fbf904f494025798
Levhav <Levhav@gmail.com>
2013-11-02 23:38:41 +1100

    v1.34

--- CometServerApi.js
+++ CometServerApi.js
@@ -23,102 +23,148 @@ function getCookie(name)
      return(setStr);
 }
 
-
-function utf8(utftext) {
-    var string = "";
-    var i = 0;
-    var c = c1 = c2 = 0;
-
-    while (i < utftext.length) {
-
-        c = utftext.charCodeAt(i);
-
-        if (c < 128) {
-            string += String.fromCharCode(c);
-            i++;
-        }
-        else if ((c > 191) && (c < 224)) {
-            c2 = utftext.charCodeAt(i + 1);
-            string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-            i += 2;
-        }
-        else {
-            c2 = utftext.charCodeAt(i + 1);
-            c3 = utftext.charCodeAt(i + 2);
-            string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-            i += 3;
-        }
-
-    }
-    return string;
-}
-
-function base64_encode( data )
-{	// Encodes data with MIME base64
-	//
-	// +   original by: Tyler Akins (http://rumkin.com)
-	// +   improved by: Bayron Guevara
-
-	var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-	var o1, o2, o3, h1, h2, h3, h4, bits, i=0, enc='';
-
-	do { // pack three octets into four hexets
-		o1 = data.charCodeAt(i++);
-		o2 = data.charCodeAt(i++);
-		o3 = data.charCodeAt(i++);
-
-		bits = o1<<16 | o2<<8 | o3;
-
-		h1 = bits>>18 & 0x3f;
-		h2 = bits>>12 & 0x3f;
-		h3 = bits>>6 & 0x3f;
-		h4 = bits & 0x3f;
-
-		// use hexets to index into b64, and append result to encoded string
-		enc += b64.charAt(h1) + b64.charAt(h2) + b64.charAt(h3) + b64.charAt(h4);
-	} while (i < data.length);
-
-	switch( data.length % 3 ){
-		case 1:
-			enc = enc.slice(0, -2) + '==';
-		break;
-		case 2:
-			enc = enc.slice(0, -1) + '=';
-		break;
+ 
+/**
+*
+*  Base64 encode / decode
+*  http://www.webtoolkit.info/
+*
+**/  
+var Base64 = {
+ 
+	// private property
+	_keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+ 
+	// public method for encoding
+	encode : function (input) {
+		var output = "";
+		var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+		var i = 0;
+ 
+		input = Base64._utf8_encode(input);
+ 
+		while (i < input.length) {
+ 
+			chr1 = input.charCodeAt(i++);
+			chr2 = input.charCodeAt(i++);
+			chr3 = input.charCodeAt(i++);
+ 
+			enc1 = chr1 >> 2;
+			enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+			enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+			enc4 = chr3 & 63;
+ 
+			if (isNaN(chr2)) {
+				enc3 = enc4 = 64;
+			} else if (isNaN(chr3)) {
+				enc4 = 64;
+			}
+ 
+			output = output +
+			this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
+			this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
+ 
+		}
+ 
+		return output;
+	},
+ 
+	// public method for decoding
+	decode : function (input) {
+		var output = "";
+		var chr1, chr2, chr3;
+		var enc1, enc2, enc3, enc4;
+		var i = 0;
+ 
+		input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+ 
+		while (i < input.length) {
+ 
+			enc1 = this._keyStr.indexOf(input.charAt(i++));
+			enc2 = this._keyStr.indexOf(input.charAt(i++));
+			enc3 = this._keyStr.indexOf(input.charAt(i++));
+			enc4 = this._keyStr.indexOf(input.charAt(i++));
+ 
+			chr1 = (enc1 << 2) | (enc2 >> 4);
+			chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+			chr3 = ((enc3 & 3) << 6) | enc4;
+ 
+			output = output + String.fromCharCode(chr1);
+ 
+			if (enc3 != 64) {
+				output = output + String.fromCharCode(chr2);
+			}
+			if (enc4 != 64) {
+				output = output + String.fromCharCode(chr3);
+			}
+ 
+		}
+ 
+		output = Base64._utf8_decode(output);
+ 
+		return output;
+ 
+	},
+ 
+	// private method for UTF-8 encoding
+	_utf8_encode : function (string) {
+		string = string.replace(/\r\n/g,"\n");
+		var utftext = "";
+ 
+		for (var n = 0; n < string.length; n++) {
+ 
+			var c = string.charCodeAt(n);
+ 
+			if (c < 128) {
+				utftext += String.fromCharCode(c);
+			}
+			else if((c > 127) && (c < 2048)) {
+				utftext += String.fromCharCode((c >> 6) | 192);
+				utftext += String.fromCharCode((c & 63) | 128);
+			}
+			else {
+				utftext += String.fromCharCode((c >> 12) | 224);
+				utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+				utftext += String.fromCharCode((c & 63) | 128);
+			}
+ 
+		}
+ 
+		return utftext;
+	},
+ 
+	// private method for UTF-8 decoding
+	_utf8_decode : function (utftext) {
+		var string = "";
+		var i = 0;
+		var c = c1 = c2 = 0;
+ 
+		while ( i < utftext.length ) {
+ 
+			c = utftext.charCodeAt(i);
+ 
+			if (c < 128) {
+				string += String.fromCharCode(c);
+				i++;
+			}
+			else if((c > 191) && (c < 224)) {
+				c2 = utftext.charCodeAt(i+1);
+				string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+				i += 2;
+			}
+			else {
+				c2 = utftext.charCodeAt(i+1);
+				c3 = utftext.charCodeAt(i+2);
+				string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+				i += 3;
+			}
+ 
+		}
+ 
+		return string;
 	}
-
-	return enc;
+ 
 }
-
-function base64_decode( data ) {
-        // Decodes data encoded with MIME base64
-	//
-	// +   original by: Tyler Akins (http://rumkin.com)
-
-	var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-	var o1, o2, o3, h1, h2, h3, h4, bits, i=0, enc='';
-
-	do {  // unpack four hexets into three octets using index points in b64
-		h1 = b64.indexOf(data.charAt(i++));
-		h2 = b64.indexOf(data.charAt(i++));
-		h3 = b64.indexOf(data.charAt(i++));
-		h4 = b64.indexOf(data.charAt(i++));
-
-		bits = h1<<18 | h2<<12 | h3<<6 | h4;
-
-		o1 = bits>>16 & 0xff;
-		o2 = bits>>8 & 0xff;
-		o3 = bits & 0xff;
-
-		if (h3 == 64)	  enc += String.fromCharCode(o1);
-		else if (h4 == 64) enc += String.fromCharCode(o1, o2);
-		else			   enc += String.fromCharCode(o1, o2, o3);
-	} while (i < data.length);
-
-	return utf8(enc);
-}
-
-
 
 function comet_server_signal()
 {
@@ -531,7 +577,7 @@ function CometServer(options)
         {
             if( rj.msg !== undefined )
             {
-                rj.msg = base64_decode(rj.msg)
+                rj.msg = Base64.decode(rj.msg)
                 try{
                     console.log(["msg", rj.msg]);
                     var pmsg = JSON.parse(rj.msg)
@@ -593,13 +639,19 @@ function CometServer(options)
          */
         this.web_pipe_send = function(pipe_name, msg)
         {
+            if( typeof msg != "string")
+	    {
+		msg = JSON.stringify(msg)
+	    }
+console.log(["web_pipe_send", pipe_name, msg])
+
             if(this.is_master)
             {
-                return this.send_msg("web_pipe\n"+pipe_name+"\n"+base64_encode(msg));
+                return this.send_msg("web_pipe\n"+pipe_name+"\n"+Base64.encode(msg));
             }
             else
             {
-                comet_server_signal().send_emit('comet_msg_slave_send_msg',"web_pipe\n"+pipe_name+"\n"+base64_encode(msg))
+                comet_server_signal().send_emit('comet_msg_slave_send_msg',"web_pipe\n"+pipe_name+"\n"+Base64.encode(msg))
             }
 
         }
@@ -828,4 +880,5 @@ function CometServer(options)
 
     return __CometServer;
 }
+
 
